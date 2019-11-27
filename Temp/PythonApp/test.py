01cabc04607d09667d4851d25a36749ccb1ad615
guojijie <guojijie@travelsky.com>
2019-11-27 15:10:45 +0800

    gx

---
+++ Temp/PythonApp/test.py
@@ -0,0 +1,6 @@
+import app
+
+attrs = "gjjjoker"
+tableName = attrs.get('__table__',None) or name
+
+print(tableName)
--- Temp/PythonApp/www/models.py
+++ Temp/PythonApp/www/models.py
@@ -7,12 +7,8 @@ Models for user, blog, comment.
 
 __author__ = 'GJJ'
 
-import time, uuid,sys,orm
+import time, uuid,sys
 
-
-sys.path.append(r'E:\临时文件\awesome-python3-webapp\Temp\PythonApp\www')
-
-from orm import Model, StringField, BooleanField, FloatField, TextField
 
 
 print('dsd')
