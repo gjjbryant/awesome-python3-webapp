088276de6f89bcc34e2898dd6a70df00da6c8462
guojijie <guojijie@travelsky.com>
2019-11-27 15:02:51 +0800

    添加实例模块

---
+++ Temp/PythonApp/www/models.py
@@ -0,0 +1,18 @@
+
+# -*- coding: utf-8 -*-
+
+'''
+Models for user, blog, comment.
+'''
+
+__author__ = 'GJJ'
+
+import time, uuid,sys,orm
+
+
+sys.path.append(r'E:\临时文件\awesome-python3-webapp\Temp\PythonApp\www')
+
+from orm import Model, StringField, BooleanField, FloatField, TextField
+
+
+print('dsd')
