b39fd8d99fc2fa4fc7687ca4f9a3ac275d2c55aa
guojijie <guojijie@travelsky.com>
2019-11-28 16:43:35 +0800

    day4 更新，增加web框架

--- www/test.py
+++ www/test.py
@@ -1,16 +1,13 @@
-import orm,asyncio
-from models import User, Blog, Comment
+# test
+import inspect
+def a(a, b=0, *c, d, e=1, **f):
+    pass
+aa = inspect.signature(a)
+print("inspect.signature（fn)是:%s" % aa)
+print("inspect.signature（fn)的类型：%s" % (type(aa)))
+print("\n")
 
-async def test(loop):
-    await orm.create_pool(loop,user='root', password='gjj123456', database='awesome')
-
-    u = User(name='Test', email='test@example.com', passwd='1234567890', image='about:blank')
-
-    await u.save()
-
-#要运行协程，需要使用事件循环 
-if __name__ == '__main__':
-        loop = asyncio.get_event_loop()
-        loop.run_until_complete(test(loop))
-        print('Test finished.')
-        loop.close()
+bb = aa.parameters
+print("signature.paramerters属性是:%s" % bb)
+print("ignature.paramerters属性的类型是%s" % type(bb))
+print("\n")
